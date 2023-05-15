182b8c4862edadffef2d7746df97291354cb71ae
stanlyshi <85752240+stanlyshi@users.noreply.github.com>
2023-05-15 22:36:42 +0800

    Update allocator.hpp

--- programming/codes/allocator.hpp
+++ programming/codes/allocator.hpp
@@ -1,6 +1,22 @@
 #pragma once
+#include <type_traits>
+#include <new>
 namespace alg
 {
+    template<class T>
+    typename std::enable_if<std::is_object<T>::value, T*>::type  addressof(T& arg) noexcept
+    {
+        return reinterpret_cast<T*>(
+            &const_cast<char&>(
+                reinterpret_cast<const volatile char&>(arg)));
+    }
+
+    template<class T>
+    typename std::enable_if<!std::is_object<T>::value, T*>::type addressof(T& arg) noexcept
+    {
+        return &arg;
+    }
+
     template <class>
     constexpr bool is_const_v = false; // determine whether type argument is const qualified
     template <class _Ty>
@@ -67,12 +83,12 @@ namespace alg
 
     template< class T >
     inline typename allocator<T>::pointer allocator<T>::address(reference x) const noexcept {
-        return &x;
+        return std::addressof(x);
     }
 
     template< class T >
     inline typename allocator<T>::const_pointer allocator<T>::address(const_reference x) const noexcept {
-        return &x;
+        return std::addressof(x);
     }
 
     template< class T >
